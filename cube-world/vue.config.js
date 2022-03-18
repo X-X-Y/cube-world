36957307f616c7d8687b4c769b61e8cdbb0650c2
X-X-Y <xxy3002@gmail.com>
2022-03-18 19:05:36 +0800

    update

--- cube-world/vue.config.js
+++ cube-world/vue.config.js
@@ -2,8 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/socket.io': {
-        // target: 'http://localhost:3000',
-        target: 'http://cube.xxy.ink:3000',
+        target: 'http://localhost:3000',
         changeOrigin: true
       }
     },
